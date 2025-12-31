d39e12194b8a312315c4df82a6411a6019500c19
jayseo <166495613+seojeongju@users.noreply.github.com>
2025-12-31 12:54:41 +0900

    Restore missing sections in Home page

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,11 +1,17 @@
 import { Hero } from '../components/Hero';
 import { TechSection } from '../components/TechSection';
+import { BusinessSection } from '../components/BusinessSection';
+import { ProductSection } from '../components/ProductSection';
+import { SupportSection } from '../components/SupportSection';
 
 export const Home = () => {
     return (
         <main>
             <Hero />
             <TechSection />
+            <BusinessSection />
+            <ProductSection />
+            <SupportSection />
         </main>
     );
 };
