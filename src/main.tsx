e3002cff5f7a4012e963f937461272ccfa76c3c1
jayseo <166495613+seojeongju@users.noreply.github.com>
2025-12-31 12:51:14 +0900

    Fix i18n by explicitly importing and providing instance in main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,14 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { I18nextProvider } from 'react-i18next';
 import './global.css'
 import App from './App.tsx'
+import i18n from './i18n';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <I18nextProvider i18n={i18n}>
+      <App />
+    </I18nextProvider>
   </StrictMode>,
 )
